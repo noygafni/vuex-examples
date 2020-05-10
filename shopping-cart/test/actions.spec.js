2ec0c5a06d3f1ce8cb4c934f55300621ca9d7ec9
Noy Gafni <noy.gafni17@gmail.com>
2020-05-10 17:53:52 +0300

    add testing to vuex store

---
+++ shopping-cart/test/actions.spec.js
@@ -0,0 +1,65 @@
+import { actions } from '../store';
+import axios from 'axios';
+
+jest.mock('axios');
+
+async function basicTest(mockGetters, times) {
+    const data = {
+        data: [
+            {
+                gtin14: 1,
+                brand_name: 'apple',
+                name: 'iphone 6',
+                size: 1000,
+            },
+            {
+                gtin14: 2,
+                brand_name: 'samsung',
+                name: 'galaxy 10',
+                size: 10,
+            },
+            {
+                gtin14: 3,
+                brand_name: 'google',
+                name: 'search',
+                size: 1,
+            },
+            {
+                gtin14: 4,
+                brand_name: 'facebook',
+                name: 'react',
+                size: 1000,
+            },
+        ]
+    }
+
+    axios.get.mockImplementationOnce(() => Promise.resolve(data));
+    const commitSpy = jest.fn();
+    await actions.fetchProducts({ commit: commitSpy, getters: mockGetters });
+    expect(commitSpy).toHaveBeenCalledTimes(times);
+}
+
+describe('actions/fetchProducts', () => {
+
+    it('should save all fetched products to store', async () => {
+        
+        const mockGetters = {
+            brandNames: []
+        }
+        await basicTest(mockGetters, 4);
+    });
+
+    it('should save only 3 fetched products to store', async () => {
+        const mockGetters = {
+            brandNames: ['apple']
+        }
+        await basicTest(mockGetters, 3);
+    });
+
+    it('should not save any fetched products to store', async () => {
+        const mockGetters = {
+            brandNames: ['apple', 'samsung', 'google', 'facebook']
+        }
+        await basicTest(mockGetters, 0);
+    })
+})
---
+++ shopping-cart/test/getters.spec.js
@@ -0,0 +1,79 @@
+import { getters } from '../store';
+
+describe('getters/brandNames', () => {
+    it('should return list of products brand names', () => {
+        const products = [
+            {
+                id: 1,
+                brand_name: 'apple',
+                name: 'iphone 6',
+                remaining: 1000,
+                price: 4000
+            },
+            {
+                id: 2,
+                brand_name: 'samsung',
+                name: 'galaxy 10',
+                remaining: 10,
+                price: 6000
+            },
+            {
+                id: 3,
+                brand_name: 'google',
+                name: 'search',
+                remaining: 1,
+                price: 0
+            },
+            {
+                id: 4,
+                brand_name: 'facebook',
+                name: 'react',
+                remaining: 1000,
+                price: 4000
+            },
+        ]
+
+        const result = ['apple', 'samsung', 'google', 'facebook'];
+        const state = { products };
+        expect(getters.brandNames(state)).toEqual(result);
+    });
+})
+
+describe('getters/totalAmount', () => {
+    it('should return the total amount of all items in cart', () => {
+        const cart = [
+            {
+                id: 1,
+                brand_name: 'apple',
+                name: 'iphone 6',
+                count: 5,
+                price: 4
+            },
+            {
+                id: 2,
+                brand_name: 'samsung',
+                name: 'galaxy 10',
+                count: 10,
+                price: 6
+            },
+            {
+                id: 3,
+                brand_name: 'google',
+                name: 'search',
+                count: 1,
+                price: 8
+            },
+            {
+                id: 4,
+                brand_name: 'facebook',
+                name: 'react',
+                count: 12,
+                price: 4
+            },
+        ]
+
+        const result = 136;
+        const state = { cart }; 
+        expect(getters.totalAmount(state)).toEqual(result);
+    })
+})
